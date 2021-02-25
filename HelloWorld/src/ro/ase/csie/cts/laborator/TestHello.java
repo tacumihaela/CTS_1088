931fb15e8f5a27bd31ee9e9ab97ddad55f90033b
Mihaela <tacumihaela18@stud.ase.ro>
2021-02-25 13:12:42 +0200

    Test project Java - Hello

---
+++ HelloWorld/src/ro/ase/csie/cts/laborator/TestHello.java
@@ -0,0 +1,14 @@
+package ro.ase.csie.cts.laborator;
+
+public class TestHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Syso ctrl space
+		System.out.println("Hello");
+			
+		
+	}
+
+}
