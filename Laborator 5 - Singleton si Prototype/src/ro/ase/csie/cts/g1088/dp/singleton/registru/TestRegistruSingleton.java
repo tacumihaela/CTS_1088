5fd9e5eec96b4f8acb8e84c94fd2d41a414f3f72
Mihaela <tacumihaela18@stud.ase.ro>
2021-03-25 13:25:58 +0200

    Final

---
+++ Laborator 5 - Singleton si Prototype/src/ro/ase/csie/cts/g1088/dp/singleton/registru/TestRegistruSingleton.java
@@ -0,0 +1,21 @@
+package ro.ase.csie.cts.g1088.dp.singleton.registru;
+
+import ro.ase.csie.cts.g1088.dp.singleton.ConexiuneBD;
+
+public class TestRegistruSingleton {
+
+	public static void main(String[] args) {
+		//ConexiuneBD conexiune1 = ConexiuneBD.getConexiune("10.0.0.1","cts");
+		//ConexiuneBD conexiune2 = ConexiuneBD.getConexiune("10.0.0.2","cts");
+	
+		
+		//if(conexiune1==conexiune2) {
+			System.out.print("Cele 2 refer sunt identice");
+			
+		//}else {
+			System.out.print("Cele 2 refer nu sunt identice");
+		}
+	}
+	
+	
+//}
