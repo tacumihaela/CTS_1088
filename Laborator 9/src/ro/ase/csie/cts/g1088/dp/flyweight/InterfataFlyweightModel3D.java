8a675798916a92dcaa658d3de0da1fa8207ed1c9
Mihaela <tacumihaela18@stud.ase.ro>
2021-04-22 12:23:31 +0300

    Flyweight

---
+++ Laborator 9/src/ro/ase/csie/cts/g1088/dp/flyweight/InterfataFlyweightModel3D.java
@@ -0,0 +1,6 @@
+package ro.ase.csie.cts.g1088.dp.flyweight;
+
+public interface InterfataFlyweightModel3D {
+	
+	public void afisareEcran(DateEcran date);
+}
---
+++ Laborator 9/src/ro/ase/csie/cts/g1088/dp/flyweight/TipModel3D.java
@@ -0,0 +1,6 @@
+package ro.ase.csie.cts.g1088.dp.flyweight;
+
+public enum TipModel3D {
+	CLADIRE, SOLDAT
+
+}
