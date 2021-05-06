57c0ba278434fae46e6beec28ffe7860f363aabe
Mihaela <tacumihaela18@stud.ase.ro>
2021-05-06 13:34:57 +0300

    Strategy State Chain

---
+++ Laborator 10/src/ro/ase/csie/cts/g1088/dp/chain/ModulProcesareMesaj.java
@@ -0,0 +1,7 @@
+package ro.ase.csie.cts.g1088.dp.chain;
+
+public abstract class ModulProcesareMesaj {
+	
+	ModulProcesareMesaj next=null;
+    public abstract void procesareMesaj(MesajChat mesaj);
+}
