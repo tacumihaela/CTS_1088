de96a1afe94e3e2288885b0cf935cb79773d8945
Mihaela <tacumihaela18@stud.ase.ro>
2021-05-20 13:30:37 +0300

    Implementare Testare Unitara

---
+++ Loborator 12 - Testare Unitara/src/ro/ase/csie/cts/g1088/testare/main/Aplicatie.java
@@ -0,0 +1,12 @@
+package ro.ase.csie.cts.g1088.testare.main;
+
+import ro.ase.csie.cts.g1088.testare.modele.OperatiiMatematice;
+
+public class Aplicatie {
+
+	public static void main(String[] args) {
+		System.out.println("Aplicatia ruleaza");
+
+	}
+
+}
